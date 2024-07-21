332f4a1562145f7b7db501e72e3a2a709795887a
Hannan-laskar <33864696+Hannan-laskar@users.noreply.github.com>
2024-07-21 16:48:02 +0530

    implemented login page

---
+++ src/login.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const login = () => {
+  return (
+    <div>
+      <form action="/action_page.php">
+        <label htmlFor="fname">First name:</label>
+        <br />
+        <input type="text" id="fname" name="fname" defaultValue="John" />
+        <br />
+        <label htmlFor="lname">Last name:</label>
+        <br />
+        <input type="text" id="lname" name="lname" defaultValue="Doe" />
+        <br />
+        <br />
+        <input type="submit" defaultValue="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default login;
